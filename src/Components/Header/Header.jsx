eb833ff8a244ba45600ce575a2600929f6f10174
YuriiKomanitskiy <55331267+yuron-maker@users.noreply.github.com>
2024-01-15 21:00:50 +0200

    Revert "Develop ivan"

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-import "./Header.scss";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import Logo from "../../Assets/logo.png";
-
+import React, { useState } from 'react';
+import './Header.scss';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../Assets/logo.png';
 
 function Header() {
   return (
@@ -15,7 +14,7 @@ function Header() {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <NavLink to="/">menu</NavLink>
-                <NavLink to="recipes">recipes</NavLink>
+                <NavLink to="link">promotions</NavLink>
                 <NavLink to="blog">blog</NavLink>
                 <NavLink to="aboutus">about us</NavLink>
                 <NavLink to="contacts">contacts</NavLink>
