d57a8014bd90dd00355ae746a3c6561bc7012abd
Danylo <danylo.babenko.bar@gmail.com>
2024-01-15 21:08:12 +0200

    i love you

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div className="container">
+      <div className="row height d-flex justify-content-center align-items-center">
+        <div className="col-md-6">
+          <div className="form">
+            <i className="fa fa-search"> </i>
+            <input
+              type="text"
+              className="form-control form-input"
+              placeholder="Search recipe..."
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
