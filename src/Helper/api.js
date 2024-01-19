154df12ceff95d1d8ede44a87cb80b3186412394
Danylo <danylo.babenko.bar@gmail.com>
2024-01-19 12:16:18 +0200

    style fix

--- src/Helper/api.js
+++ src/Helper/api.js
@@ -23,9 +23,7 @@ function request(url, method, data) {
           .then((response) => response);
 
       case 'DELETE':
-        return axios
-          .delete(`${BASE_URL}${url}`)
-          .then((response) => response);
+        return axios.delete(`${BASE_URL}${url}`).then((response) => response);
 
       default:
         return null;
