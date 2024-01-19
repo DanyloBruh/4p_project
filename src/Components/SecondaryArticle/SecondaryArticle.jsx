9802c229bbdb16f65145a040dfa26d9cf3e24242
Danylo <danylo.babenko.bar@gmail.com>
2024-01-19 13:07:05 +0200

    connect blog to api

--- src/Components/SecondaryArticle/SecondaryArticle.jsx
+++ src/Components/SecondaryArticle/SecondaryArticle.jsx
@@ -1,25 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Borch from '../../Assets/borsch.jpg';
 import './SecondaryArticle.scss';
 
-function SecondaryArticle() {
+function SecondaryArticle({
+  id, title, text, createdBy, createdAt,
+}) {
   return (
-    <Link to={`/blog/${5987}`}>
+    <Link to={`/blog/${id}`}>
       <div className="secondary-article-content">
         <img src={Borch} alt="" />
-        <h2>
-          Delightful Ukrainian Fare: A Journey Through Traditional Cuisine
-        </h2>
-        <p>
-          Experience the rich tapestry of Ukrainian culinary traditions with our
-          immersive journey through this vibrant cuisine. From hearty borscht to
-          the beloved varenyky, immerse yourself in the flavors that define
-          Ukrainian gastronomy...
-        </p>
+        <h2>{title}</h2>
+        <p>{text}</p>
         <div>
-          <p>Ava Martinez</p>
-          <p>12/13/22</p>
+          <p>{createdBy}</p>
+          <p>{createdAt}</p>
         </div>
       </div>
     </Link>
--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import MainArticle from '../../Components/MainArticle/MainArticle';
 import './Blog.scss';
 import SecondaryArticle from '../../Components/SecondaryArticle/SecondaryArticle';
 import TextArticle from '../../Components/TextArticle/TextArticle';
+import { getAllBlogs } from '../../Helper/requests';
 
 function Blog() {
+  const [allBlogs, setAllBlogs] = useState();
+
+  useEffect(() => {
+    getAllBlogs().then(setAllBlogs);
+  }, []);
+  console.log(allBlogs);
   return (
     <div className="blog__content">
       <Container>
@@ -18,24 +25,17 @@ function Blog() {
           </Col>
         </Row>
         <Row className="blog__secondary-article">
-          <Col lg={4}>
-            <SecondaryArticle />
-          </Col>
-          <Col lg={4}>
-            <SecondaryArticle />
-          </Col>
-          <Col lg={4}>
-            <SecondaryArticle />
-          </Col>
-          <Col lg={4}>
-            <SecondaryArticle />
-          </Col>
-          <Col lg={4}>
-            <SecondaryArticle />
-          </Col>
-          <Col lg={4}>
-            <SecondaryArticle />
-          </Col>
+          {allBlogs?.map((item) => (
+            <Col key={item.id} lg={4}>
+              <SecondaryArticle
+                id={item.id}
+                title={item.name}
+                text={item.text}
+                createdBy={item.User.name}
+                createdAt={item.createdAt}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
       <div className="ornament-left" />
