6e7f615ca7cead2a10a78fada59b02748196bc61
Danylo <danylo.babenko.bar@gmail.com>
2024-01-22 18:59:27 +0200

    mobile adaptive header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,10 @@ import './Header.scss';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import Logo from '../../Assets/logo.png';
+import SideBar from '../SideBar/SideBar';
 
 function Header() {
+  const [openSideBar, setOpenSideBar] = useState(false);
   const [color, setColor] = useState(false);
 
   const changeColor = () => {
@@ -20,20 +22,30 @@ function Header() {
     <div className={`header ${color ? 'header__bg-black' : 'header__bg'}`}>
       <Container className="header__container">
         <img src={Logo} alt="logo" />
-        <Navbar data-bs-theme="dark">
-          <Container>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <NavLink to="/">menu</NavLink>
-                <NavLink to="recipes">recipes</NavLink>
-                <NavLink to="blog">blog</NavLink>
-                <NavLink to="aboutus">about us</NavLink>
-                <NavLink to="contacts">contacts</NavLink>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
+        <Navbar data-bs-theme="dark" className="header__navbar">
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink to="/">menu</NavLink>
+              <NavLink to="recipes">recipes</NavLink>
+              <NavLink to="blog">blog</NavLink>
+              <NavLink to="aboutus">about us</NavLink>
+              <NavLink to="contacts">contacts</NavLink>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
+        <button
+          type="button"
+          className={`header__hamburger ${openSideBar ? 'open-hamburger' : ''}`}
+          aria-label="Side bar"
+          onClick={() => setOpenSideBar((prev) => !prev)}
+        >
+          <span> </span>
+          <span> </span>
+          <span> </span>
+          <span> </span>
+        </button>
       </Container>
+      <SideBar openSideBar={openSideBar} setOpenSideBar={setOpenSideBar} />
     </div>
   );
 }
