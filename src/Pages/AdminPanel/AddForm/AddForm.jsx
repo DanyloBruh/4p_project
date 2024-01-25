35dc276de7287c29f1aa0f5889b3fb5b6f1cd245
joovankoo <joovankoo@gmail.com>
2024-01-25 21:35:54 +0200

    import uuid | can add user to database without validation :(

--- src/Pages/AdminPanel/AddForm/AddForm.jsx
+++ src/Pages/AdminPanel/AddForm/AddForm.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
 import './AddForm.scss';
 
 function AddForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    role: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   const category = useLocation().pathname.split('/')[2];
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log('id: ', uuidv4(), formData);
+    axios.post('http://localhost:3005/user/', { id: uuidv4(), ...formData });
     navigate(`/admin/${category}`);
   };
 
@@ -22,23 +42,41 @@ function AddForm() {
         <form>
           <div className="form-element">
             <p>Name</p>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={handleInputChange}
+              value={formData.name}
+              name="name"
+            />
           </div>
           <div className="form-element">
-            <p>description</p>
-            <input type="text" />
+            <p>role</p>
+            <select
+              onChange={handleInputChange}
+              value={formData.role}
+              name="role"
+            >
+              <option>admin</option>
+              <option>employee</option>
+            </select>
           </div>
           <div className="form-element">
-            <p>weight</p>
-            <input type="text" />
+            <p>email</p>
+            <input
+              type="text"
+              onChange={handleInputChange}
+              value={formData.email}
+              name="email"
+            />
           </div>
           <div className="form-element">
-            <p>price</p>
-            <input type="number" />
-          </div>
-          <div className="form-element">
-            <p>ingredients</p>
-            <input type="text" />
+            <p>password</p>
+            <input
+              type="password"
+              onChange={handleInputChange}
+              value={formData.password}
+              name="password"
+            />
           </div>
           <br />
           <Button variant="outline-light" onClick={handleSubmit}>
