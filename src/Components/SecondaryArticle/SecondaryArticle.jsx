a79c0e2ed2c8b1b808c0f1da64468e20c6c5230b
Danylo <danylo.babenko.bar@gmail.com>
2024-02-08 14:24:27 +0200

    fix blog with html to react

--- src/Components/SecondaryArticle/SecondaryArticle.jsx
+++ src/Components/SecondaryArticle/SecondaryArticle.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import HtmlToReactParser from 'html-to-react';
 import { Link } from 'react-router-dom';
 import './SecondaryArticle.scss';
 
@@ -12,6 +14,8 @@ function SecondaryArticle({
   image,
   imageName,
 }) {
+  const Parser = new HtmlToReactParser.Parser();
+
   return (
     <Link to={`/blog/${id}`}>
       <div className="secondary-article-content">
@@ -20,7 +24,9 @@ function SecondaryArticle({
         </div>
 
         <h2>{title}</h2>
-        <p>{text}</p>
+        <div className="secondary-article-content__text">
+          {Parser.parse(text)}
+        </div>
         <div>
           <p>{createdBy}</p>
           <p>{createdAt}</p>
