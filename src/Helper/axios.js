9622d5cd08413734492b0d9e5ea075dbe56e6bb8
root <root@ip-172-31-28-67.eu-west-2.compute.internal>
2024-03-06 10:52:51 +0000

    server com

--- src/Helper/axios.js
+++ src/Helper/axios.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'https://www.4rompeople.com:99',
   withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'https://www.4rompeople.com:99',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
 
 export const axiosPrivateConfig = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'https://www.4rompeople.com:99',
   headers: { 'Content-Type': 'multipart/form-data' },
   withCredentials: true,
 });
